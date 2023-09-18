9630f2858b30ad1b7c24aa6a1684d4bf9c545387
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2023-09-19 10:09:35 +1200

    Impact Page
    CSS Styling for the HeroSlide (Carousel)

--- src/componets/HeroSlide.jsx
+++ src/componets/HeroSlide.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import volunteeringwork1 from '/Users/abdul-qudusamidu/Documents/React-ASHS-Volunteering-Hub/ashs-volunteering-part-5/src/assests/volunteering-work.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import volunteeringwork2 from 'src/assests/volunteering-work-2.jpg';
-//import volunteeringwork3 from 'src/assests/volunteering-work-3.jpg';
-//import volunteeringwork4 from 'src/assests/volunteering-work-4.jpg';
+import volunteeringwork2 from '/Users/abdul-qudusamidu/Documents/React-ASHS-Volunteering-Hub/ashs-volunteering-part-5/src/assests/volunteering-work-2.jpg';
+import volunteeringwork3 from '/Users/abdul-qudusamidu/Documents/React-ASHS-Volunteering-Hub/ashs-volunteering-part-5/src/assests/volunteering-work-3.jpg';
+import volunteeringwork4 from '/Users/abdul-qudusamidu/Documents/React-ASHS-Volunteering-Hub/ashs-volunteering-part-5/src/assests/volunteering-work-4.jpg';
+import "./HeroSlide.css"
 
 export const HeroSlide = () => {
     return (
@@ -18,34 +19,42 @@ export const HeroSlide = () => {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>Coaching and Volunteering for a Purpose</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
 
-                <Carousel.Item interval={500}>
+                <Carousel.Item interval={1000}>
                     <img
                         className="d-block w-100"
-                        src={volunteeringwork1}
+                        src={volunteeringwork2}
                         alt="Second slide"
                     />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>Coaching and Volunteering for a Purpose</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
 
-                <Carousel.Item>
+                <Carousel.Item interval={1000}>
                     <img
                         className="d-block w-100"
-                        src={volunteeringwork1}
+                        src={volunteeringwork3}
                         alt="Third slide"
                     />
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3>Coaching and Volunteering for a Purpose</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src={volunteeringwork4}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Coaching and Volunteering for a Purpose</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
