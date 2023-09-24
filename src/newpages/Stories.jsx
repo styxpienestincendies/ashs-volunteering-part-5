3c50146b0bc6e44046bc8d42ecdac2b1c05ab7e3
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2023-09-25 12:58:47 +1300

    Update on Stories

--- src/newpages/Stories.jsx
+++ src/newpages/Stories.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Stories = () => {
     return (
         <div>
-            Stories
+            Regrettably, due to time constraints, the completion of this webpage was postponed.
+            Nevertheless, its fundamental objective remains intact: to provide registered users with the
+            ability to author insightful blog posts, recounting their experiences with volunteer endeavors.
+            Furthermore, this platform is designed to allow contributors to offer valuable perspectives on the
+            strategies and methods they employed to accomplish their tasks.
         </div>
     )
 }
