46d800d6c90920e8565b208dfa4dbf57657e41b4
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2023-09-25 13:21:49 +1300

    Update Forum page

--- src/newpages/Forum.jsx
+++ src/newpages/Forum.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export const Forum = () => {
     return (
         <div>
-            Forum
+            Regrettably, due to time constraints, the completion of this webpage was postponed. The fudamental objective of this webpage was to provide
+            a forum page so that users that where in need of volunteering help can post their issues and what they need help with; in which
+            a dedicated volunteer will be able to see it and reply giving them insight on what to do or rather helping them in which they're able to dicuss
+            personally on how they would move forwards after they agreed to help. So basically the forum page is to get help with potential volunteering work or get advice if needs be.
         </div>
     )
 }
