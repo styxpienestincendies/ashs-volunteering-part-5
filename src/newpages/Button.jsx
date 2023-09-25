2383e358604ea9251bd93827a8d2a1f1fc4d03c0
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2023-09-25 14:06:01 +1300

    Update Button

--- src/newpages/Button.jsx
+++ src/newpages/Button.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export const Button = () => {
     return (
         <div>
-            Button
+            This serve to act as a button, which if the user end up being interested in this webite and want to further persue
+            what it's to be a volunteer, they can click the button "Volunteer", which woold've lead them to a sign-up and log-in page,
+            which they would be able to create an account or log into an existing account. This would be possible using firebase as a
+            database to store the user informations, which would be kept safe.
         </div>
     )
 }
